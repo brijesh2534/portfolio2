a72a973cc8aa28e4df754f996b1103b9dd0c2962
brijesh <brijeshbrijesh36262@gmail.com>
2025-08-16 10:58:33 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,28 +6,15 @@
 //   },
 //   plugins: [],
 // };
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     './index.html',
-//     './admin.html', // Add this line
-//     './src/**/*.{js,ts,jsx,tsx}'
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        admin: 'admin.html',
-      },
-    },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './admin.html', // Add this line
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
   },
-});
+  plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,35 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//         tailwindcss(),
+//   ],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        admin: './admin.html',
+      },
+    },
   },
 });
