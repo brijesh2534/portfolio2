b94d383a8b5ef5046943a8901c188e403934893f
brijesh <brijeshbrijesh36262@gmail.com>
2025-08-16 10:43:49 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './admin.html', // Add this line
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
